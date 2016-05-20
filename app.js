e83e32b07ae99f9c5eb109cb0fe5501ef758d3a7
Heikki Kurhinen <heikki.kurhinen@gmail.com>
2016-05-20 08:36:11 +0300

    ** Fixed formatting

--- app.js
+++ app.js
@@ -11,57 +11,57 @@ var fs = require('fs');
 
 var SERVER_ROOT = '';
 
-var clientTemplates = fs.readdirSync(__dirname+'/views/client');
+var clientTemplates = fs.readdirSync(__dirname + '/views/client');
 var compiledClientTemplates = [];
 compiledClientTemplates.push(
-  fs.readFileSync(__dirname+'/node_modules/jade/runtime.js', {encoding: 'utf8'})
+  fs.readFileSync(__dirname + '/node_modules/jade/runtime.js', { encoding: 'utf8' })
 );
-for(var i = 0; i < clientTemplates.length;i++){
+for (var i = 0; i < clientTemplates.length; i++) {
   compiledClientTemplates.push(jade.compileFileClient(
-    __dirname+'/views/client/'+clientTemplates[i],
-    {name: clientTemplates[i].replace('.jade', '')}
+    __dirname + '/views/client/' + clientTemplates[i],
+    { name: clientTemplates[i].replace('.jade', '') }
   ));
 }
-fs.writeFileSync(__dirname+'/public/js/templates.js', compiledClientTemplates.join(''));
+fs.writeFileSync(__dirname + '/public/js/templates.js', compiledClientTemplates.join(''));
 
 mongoose.connect(database.getConnectionUrl());
 var datastore = mongoose.connection;
 
 datastore.on("error", function(err) {
-	console.error('Failed to connect to DB, exiting...');
+  console.error('Failed to connect to DB, exiting...');
 });
 
-datastore.on('disconnected', function () {
-	console.error('Mongoose connection to DB disconnected');
+datastore.on('disconnected', function() {
+  console.error('Mongoose connection to DB disconnected');
 });
 
-var closeConnection = function(){
-	datastore.close(function () {
-		console.log('Mongoose default connection with DB disconnected, because application was closed.');
-		process.exit(0);
-	});
+var closeConnection = function() {
+  datastore.close(function() {
+    console.log('Mongoose default connection with DB disconnected, because application was closed.');
+    process.exit(0);
+  });
 };
 
 process.on('SIGINT', closeConnection).on('SIGTERM', closeConnection);
 
 datastore.once('open', function() {
-	
-	var app = express();
 
-	app.set('port', process.env.PORT || 3000);
-	app.set('views', path.join(__dirname, 'views'));
-	app.set('view engine', 'jade');
-	app.use(express.static(path.join(__dirname, 'public')));
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded());
-	
-	app.get(SERVER_ROOT+'/', routes.index);
-	app.get(SERVER_ROOT+'/feedback', routes.getFeedback);
-	
-	app.post(SERVER_ROOT+'/feedback', routes.addFeedback);
-	app.post(SERVER_ROOT+'/comment/:id', routes.addComment);
+  var app = express();
 
-	http.createServer(app).listen(app.get('port'), function(){
-	  console.log('Express server listening on port ' + app.get('port'));
-	});
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded());
+
+  app.get(SERVER_ROOT + '/', routes.index);
+  app.get(SERVER_ROOT + '/feedback', routes.getFeedback);
+
+  app.post(SERVER_ROOT + '/feedback', routes.addFeedback);
+  app.post(SERVER_ROOT + '/comment/:id', routes.addComment);
+
+  http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+  });
 });
